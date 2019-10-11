fd0fee7e68e3dd2fb55ed08b45ee7bd4a5ad77f2
Joshua Young <joshua.r.young1@gmail.com>
2019-10-10 18:46:41 -0600

    adds cron stuff

--- jeff-bridger.php
+++ jeff-bridger.php
@@ -30,26 +30,12 @@
 
 
 
-
-
 register_activation_hook( __FILE__, 'onActivate');
 register_deactivation_hook( __FILE__, 'onDeactivate' );
-// add_action('woocommerce_order_status_pending_to_processing', 'order_status_change', 10, 2);
-add_action('woocommerce_order_status_pending', 'order_status_change');
-add_action('woocommerce_order_status_processing', 'order_status_change');
-add_action('woocommerce_order_status_on_hold', 'order_status_change');
-add_action('woocommerce_order_status_cancelled', 'order_status_change');
-add_action('woocommerce_order_status_completed', 'order_status_change');
-add_action('woocommerce_order_status_refunded', 'order_status_change');
-add_action('woocommerce_order_status_failed', 'order_status_change');
 
-add_action('woocommerce_order_edit_status', 'order_edit_status', 10, 2);
-
-add_action('woocommerce_process_shop_order_meta', 'order_meta', 10, 2);
 add_action('admin_menu', 'create_menu');
-
-// FOR DEV ON THE CRON CALLBACK
-add_action('woocommerce_after_register_post_type', 'jeff_cron_callback');
+add_filter( 'cron_schedules', 'add_cron_interval');
+add_action('plugins_loaded', 'jeff_cron_init');
 
 add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'add_plugin_page_settings_link');
 
@@ -58,10 +44,6 @@ function add_plugin_page_settings_link( $links ) {
 		admin_url( 'admin.php?page=jeff-bridger%2Fjeff-bridger.php' ) .
 		'">' . __('Settings') . '</a>';
 	return $links;
-}
-
-function order_edit_status($order_id, $to) {
-  wpLog(['$order_id'=>$order_id, '$to'=>$to]);
 }
 
 function onActivate() {
@@ -78,14 +60,12 @@ function order_status_change($order_id) {
 
 function jeff_sync($order_id) {
   $base_jeff_url = get_option('jb_base_url');
-  $uri = $base_jeff_url . $order_id;
+  $uri = $base_jeff_url . '/syncOrders/' . $order_id;
 
   wpLog(['$uri'=>$uri]);
 
-  // $order = new WC_order($order_id);
-  // wpLog(['$order'=>$order->get_data()]);
-
   $res = wp_remote_get($uri);
+  wpLog(['$res'=>$res]);
   return $res;
 }
 
@@ -101,14 +81,25 @@ function jeff_cron_init() {
   $cron_job_exists = wp_next_scheduled( 'jeff_cron_event');
   // wpLog(['$cron_job_exists'=>$cron_job_exists]);
   if(!$cron_job_exists) {
+    wpLog('Cron doesn\'t exist');
+    // $schedules = wp_get_schedules();
     // wpLog(['wp_get_scheduled_event'=>'jeff_cron_event not found. Scheduling now!']);
-    $cron_array = wp_schedule_event( time(), 'hourly', 'jeff_cron_event');
+    // wpLog(['$schedules'=>$schedules]);
+    $cron_array = wp_schedule_event( time(), 'sixty_seconds', 'jeff_cron_event');
   }
-
-  // $scheduled_jobs = _get_cron_array();
-  // wpLog(['$scheduled_jobs'=>$scheduled_jobs]);
-
   add_action('jeff_cron_event', 'jeff_cron_callback');
+
+  $scheduled_jobs = _get_cron_array();
+  // wpLog(['$scheduled_jobs'=>$scheduled_jobs]);
+}
+
+function add_cron_interval( $schedules ) {
+    $schedules['sixty_seconds'] = array(
+        'interval' => 60,
+        'display'  => esc_html__( 'Every Minute' ),
+    );
+ 
+    return $schedules;
 }
 
 function jeff_cron_callback() {
@@ -118,19 +109,62 @@ function jeff_cron_callback() {
   ];
   //Get woo orders
   $woo_orders = wc_get_orders($args);
-  $woo_order_data_arr = array_map(function ($order){
+  $woo_orders = array_map(function ($order){
     return $order->get_data();
   }, $woo_orders);
-  // wpLog(['$woo_order_data_arr'=>$woo_order_data_arr]);
+  $woo_ids_to_index = array_map(function($order) {
+    return $order['id'];
+  }, $woo_orders);
+  $woo_ids_to_index = array_flip($woo_ids_to_index);
+  // wpLog(['$woo_ids_to_index'=>$woo_ids_to_index]);
+  
   //Get airtable orders
+  $base_jeff_url = get_option('jb_base_url');
+  $airtable_orders = wp_remote_retrieve_body(wp_remote_get($base_jeff_url . '/getAirtableOrders'));
+  $airtable_orders = json_decode($airtable_orders, true);
+  $airtable_orders = array_map(function ($order){
+    return $order['fields'];
+  }, $airtable_orders);
+  $airtable_ids_to_index = array_map(function($order) {
+    $woo_order_id = $order['wooOrderId'];
+    return $woo_order_id ? $woo_order_id : -1;
+  }, $airtable_orders);
+  // wpLog(['$airtable_ids_to_index before'=>$airtable_ids_to_index]);
+  $airtable_ids_to_index = array_flip($airtable_ids_to_index);
+  // wpLog(['$airtable_ids_to_index after'=>$airtable_ids_to_index]);
 
   //Compare orders
-
   //If there are discrepancies, sync
-  foreach($woo_order_data_arr as $order) {
+  $discrepencies = [];
+  foreach($woo_orders as $order) {
     $id = $order['id'];
-    // $res = jeff_sync($id);
-    wpLog(['$id'=>$id]);
+    $status = $order['status'];
+
+    if($status !== $airtable_orders[$airtable_ids_to_index[$id]]['status']) {
+      // wpLog([
+      //   $id=>'Status mismatch!',
+      //   'refunds'=>$refunds,
+      //   '$order'=>$order,
+      //   '$id'=>$id,
+      //   '$status'=>$status,
+      //   '$airtable_ids_to_index[$id]'=>$airtable_ids_to_index[$id]
+      // ]);
+      $discrepencies[] = $id;
+    }
+  }
+  // wpLog(['$discrepencies'=>$discrepencies]);
+  
+  //Limit it to 25 at a time
+  $index = 0;
+  while($index < 25) {
+    if($discrepencies[$index]) {
+      $res = jeff_sync($discrepencies[$index]);
+      $res = wp_remote_retrieve_body($res);
+      // wpLog(['$res'=>$res]);
+      $index++;
+    } else {
+      $index = 25;
+    }
   }
 }
 
